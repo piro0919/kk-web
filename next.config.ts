6b184b81a035f41ba02348a44ae3af6f23080e83
piro0919 <piro.haniwa@gmail.com>
2025-07-15 04:56:28 +0900

    feat: skip log-rocket in dev; allow app.logrocket.com

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["app.logrocket.com"],
   experimental: {
     typedEnv: true,
     // typedRoutes: true,
