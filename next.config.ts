4c93d1af512695dd4003b8e596875e0247331684
piro0919 <piro.haniwa@gmail.com>
2025-09-06 21:02:30 +0900

    feat(config): enable react-compiler and add babel-plugin-react-compiler

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["app.logrocket.com"],
   experimental: {
+    reactCompiler: true,
     typedEnv: true,
     useLightningcss: true,
     // typedRoutes: true,
